d78fdf2f235b8ef3bb992579a755dafeb640764f
EnzoGabrielFS <108841425+EnzoGabrielFS@users.noreply.github.com>
2022-08-22 20:49:04 -0300

    Create Candidatos.java

---
+++ scr/model/Candidatos.java
@@ -0,0 +1,15 @@
+package model;
+
+import java.util.Random;
+
+public class Candidato extends Pessoa {
+
+	public Candidato(String nome) {
+		super(nome);
+	}
+	
+	public boolean apresentaCandidatura() {
+		return new Random().nextBoolean();
+	}
+
+}
