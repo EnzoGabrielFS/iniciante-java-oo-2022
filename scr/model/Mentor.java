c6b8d128502118dc2067d2ff554814674d370143
EnzoGabrielFS <108841425+EnzoGabrielFS@users.noreply.github.com>
2022-08-22 21:31:18 -0300

    Create Mentor.java

---
+++ scr/model/Mentor.java
@@ -0,0 +1,16 @@
+package model;
+
+import java.util.List;
+
+public class Mentor extends Pessoa implements Organizador {
+
+	public Mentor(String nome, List<String> conhecimento) {
+		super(nome);
+		super.setConhecimento(conhecimento);
+	}
+	
+	public void tiraDuvida(String duvida, Turma turma) {
+		turma.compartilhaExplicacao(duvida);
+		System.out.println("Mentor " + super.getNome() + " tirou dúvida " + duvida + " da turma");
+	}
+}
