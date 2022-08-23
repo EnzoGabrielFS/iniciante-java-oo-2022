54cedde0cf9242faa9c2a95e16b38e06a7a2fe43
EnzoGabrielFS <108841425+EnzoGabrielFS@users.noreply.github.com>
2022-08-22 21:15:50 -0300

    Create Entrevistas.java

---
+++ scr/model/Entrevistas.java
@@ -0,0 +1,23 @@
+package model;
+
+import java.util.Date;
+
+public class Entrevista extends Evento {
+
+	private Recrutador recrutador;
+	
+	private Candidato candidato;
+	
+	public Entrevista(Recrutador recrutador, Candidato candidato, Date dataEHoraMarcada) {
+		super("Entrevista com " + candidato.getNome(), dataEHoraMarcada, recrutador);
+		this.recrutador = recrutador;
+		this.candidato = candidato;
+		super.getParticiipantes().add(candidato);
+	}
+
+	@Override
+	public void executa() {
+		recrutador.avaliaCandidato(candidato);
+	}
+	
+}
